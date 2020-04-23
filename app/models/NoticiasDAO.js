27f2b7683f3f7446050d1db7bb5e0453b7a08652
Danilo Torres <danilo.costa@ccc.ufcg.edu.br>
2020-04-22 22:45:14 -0300

    atualizando o modulo mysql do projeto
    atualizando o modulo mysql do projeto

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -8,9 +8,9 @@ NoticiasDAO.prototype.getNoticias = function(callback){
 	this._connection.query('select * from noticias order by data_criacao desc', callback);
 }
 
-NoticiasDAO.prototype.getNoticia = function(id_noticia, callback){
-	console.log(id_noticia.id_noticia);
-	this._connection.query('select * from noticias where id_noticia = ' + id_noticia.id_noticia, callback);
+NoticiasDAO.prototype.getNoticia = function(id_noticias, callback){
+	console.log(id_noticias.id_noticias);
+	this._connection.query('select * from noticias where id_noticias = ' + id_noticias.id_noticias, callback);
 }
 
 NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
--- sql/insert_noticias.sql
+++ sql/insert_noticias.sql
@@ -1,3 +1,4 @@
 INSERT INTO noticias (titulo, noticia) VALUES ('titulo da noticia', 'conteudo da noticia');
+INSERT INTO noticias (titulo, noticia) VALUES ('outra noticia', 'conteudo da outra noticia');
 
 COMMIT;
