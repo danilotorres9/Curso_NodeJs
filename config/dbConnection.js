58aecdf59f8b31efc6f36c37b856b0d55e79924f
Danilo Torres <danilo.costa@ccc.ufcg.edu.br>
2020-05-04 22:15:46 -0300

    remocao do plugin mysql

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connMySQL = function(){
 	return mysql.createConnection({
 		host : 'localhost',
-		user : 'danilotorres9',
+		user : 'root',
 		password : '14jan2000',
 		database : 'portal_noticias'
 	});
